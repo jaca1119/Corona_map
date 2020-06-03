525e568b7fb70e4c40be9612ee0a9d3f616b42bf
jaca1119 <laki1663@gmail.com>
2020-06-03 14:36:37 +0200

    Added eureka discovery client

--- src/main/java/com/itvsme/corona/CoronaApplication.java
+++ src/main/java/com/itvsme/corona/CoronaApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class CoronaApplication
 {
 
